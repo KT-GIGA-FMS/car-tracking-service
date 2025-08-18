c92a929d228af87b03a67d21094adfabbe8f2937
rlfrkdms1 <rlfrkdms1@naver.com>
2025-08-18 17:19:15 +0900

    [FEAT]: Spring 프로젝트 세팅

---
+++ src/main/java/com/kt_giga_fms/car_tracking/CarTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.kt_giga_fms.car_tracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarTrackingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kt_giga_fms/car_tracking/CarTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kt_giga_fms.car_tracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
